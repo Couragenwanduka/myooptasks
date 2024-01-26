4dbd58633a9f0f2518a81b0bf410a243d69830d7
Couragenwanduka <156565155+Couragenwanduka@users.noreply.github.com>
2024-01-26 22:29:42 +0100

    Update simplemovierentingapi.js

--- simplemovierentingapi.js
+++ simplemovierentingapi.js
@@ -1,39 +1,38 @@
 class MovieRentingAPI{
   constructor(){
-    this.movies=movies;
     this.loginArr=loginArr;
-    this.userEmail= userEmail;
-    this.userPassword=userPassword;
     this.userEmailInput=userEmailInput;
     this.userPasswordInput= userPasswordInput;
-    this.userName= userName;
-    this.currentDate= currentDate;
-    this.newDate= newDate;
-    this.theMovie= theMovie;
-  }
+    }
  signup(){
  this.loginArr.push(this.userEmailInput);
  this.loginArr.push(this.userPasswordInput);
- 
  }
  login(){
   if(this.loginArr.includes(this.userEmailInput) && this.loginArr.includes(this.userPasswordInput)){
     console.log(`welcome back ${this.userName}`)
    }else{console.log("no")}
  }
- rentMovie(){
-  if(this.movies.includes(this.theMovie)){
-    console.log(` ${this.theMovie} was rented by ${this.userName} on${this.newDate}`)
-  }else{ console.log("the movie isn't available")}
  }
-returnmovie(){
-  if(this.movies.includes(this.theMovie)){
-    console.log(` ${this.theMovie} was returned by ${this.userName} on${this.newDate}`)
+class Renting{
+  constructor(){
+    this.movies=movies;
+    this.userName= userName;
+    this.currentDate= currentDate;
+    this.newDate= newDate;
+    this.theMovie= theMovie;
   }
- 
-}
-
-}
+  rentMovie(){
+    if(this.movies.includes(this.theMovie)){
+      console.log(` ${this.theMovie} was rented by ${this.userName} on${this.newDate}`)
+    }else{ console.log("the movie isn't available")}
+   }
+  returnmovie(){
+    if(this.movies.includes(this.theMovie)){
+      console.log(` ${this.theMovie} was returned by ${this.userName} on${this.newDate}`)
+    } 
+  }
+ }
 // Get the current date and time
 const currentDate = new Date();
 
@@ -48,11 +47,12 @@ const userPassword=[];
 const userEmail=[];
 const loginArr=[];
 const movies=["pose","wednesday","young sheldon","blood&water", "adire", "lucifer","nerver have i ever","top boy","suits","flash","Baki","60 minutes","12 strong"]
+const second= new Renting()
 const myMovie = new MovieRentingAPI(userEmailInput,userPasswordInput,movies,userPassword,userEmail);
 myMovie.signup()
 myMovie.login()
-myMovie.rentMovie()
-myMovie.returnmovie()
+second.rentMovie()
+second.returnmovie()
 
 
 
