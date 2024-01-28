bbf4a5b2435ca5b70dd36344b82b4c9826c1ed6c
Couragenwanduka <156565155+Couragenwanduka@users.noreply.github.com>
2024-01-28 18:55:48 +0100

    Update classes.js

--- classes.js
+++ classes.js
@@ -1,24 +1,22 @@
-// defining a  class
-class courage {
-  name= "Courage Nduka";
-  age= 23;
-  height= "6ft";
-  eyecolour= "brown";
-  complextion="dark skined";
-  weight="90kg";
-  static skills= "zero skills";
-  static finacials= "broke"
-
-  handsomeBoy(){
-    console.log(`My name is ${this.name} I am ${this.age} years old, ${this.height},${this.complextion}  with ${this.eyecolour} eyes and I way ${this.weight} `)
+class Introduction{
+  static name=this.name
+  constructor(name,age,height,complextion,weight){
+    this.name=name;
+    this.age=age;
+    this.height=height;
+    this.complextion=complextion;
+    this.weight=weight;
   }
-  
- static redFlags(){
-    console.log(`redFlags are i have ${courage.skills} and I am currently ${courage.finacials} `)
-  }
+ mySelf(){
+  console.log(`My name is ${this.name} I am ${this.age} years old I am ${this.height} tall and I have a ${this.complextion} and I way ${this.weight}`)
+ }
 
+static hobbies(name){
+  this.name=name
+  console.log(` ${this.name}  likes to sing and read`);
 }
-//instanstiate an object 
-const chidi= new courage()
-chidi.handsomeBoy()
-courage.redFlags()
+}
+
+const user1= new Introduction("Courage","23","6ft","dark complextion","90kg")
+Introduction.hobbies("Courage")
+user1.mySelf()
